f58d40d17ef4a8608b5b56ca7e3d3d452486e8fc
Krishani-imalka <159682806+Krishani-imalka@users.noreply.github.com>
2024-10-18 22:01:16 +0530

    UPDATEs

--- STRUCTURE OF TABLES.sql
+++ STRUCTURE OF TABLES.sql
@@ -1,6 +1,6 @@
 
 USER(
- User_Id  CHAR(20),
+ User_Id  VARCHAR(20),
  NIC      CHAR(20),
  F_name  VARCHAR(50),
  L_name  VARCHAR(50),
@@ -13,8 +13,8 @@ USER(
 );
 ----------------------------------------------
 STUDENT(
- Stu_ID   CHAR(20),
- S_Status CHAR(20),
+ Stu_ID   VARCHAR(20),
+ S_Status VARCHAR(20),
  GPA   DECIMAL(1,2),
  PRIMARY KEY(Stu_ID),
  FOREIGN KEY(Stu_ID) REFERENCES USER(User_Id),
@@ -26,7 +26,7 @@ STUDENT(
 --------------------------------------------
 
 LECTURER(
- LEC_ID   CHAR(20),
+ LEC_ID   VARCHAR(20),
  Position VARCHAR(50),
  PRIMARY KEY(LEC_ID),
  FOREIGN KEY(LEC_ID) REFERENCES USER(User_Id)  
@@ -35,7 +35,7 @@ LECTURER(
 ----------------------------------------------
 
 ADMIN(
- ADMIN_ID CHAR(20),
+ ADMIN_ID VARCHAR(20),
  A_Role   VARCHAR(50),
  PRIMARY KEY(ADMIN_ID)
  FOREIGN KEY(ADMIN_ID) REFERENCES USER(User_Id)
@@ -43,7 +43,7 @@ ADMIN(
 ----------------------------------------------
 
 DEAN(
- DEAN_ID CHAR(20),
+ DEAN_ID VARCHAR(20),
  PRIMARY KEY(DEAN_ID),
  FOREIGN KEY(DEAN_ID) REFERENCES USER(User_Id)
 
@@ -51,7 +51,7 @@ DEAN(
 ----------------------------------------------
 
 TO(
- TO_ID   CHAR(20),
+ TO_ID   VARCHAR(20),
  T_Role  VARCHAR(50),
  PRIMARY KEY(TO_ID),
  FOREIGN KEY(TO_ID) REFERENCES USER(User_Id)
@@ -59,8 +59,8 @@ TO(
 ---------------------------------------------
 
 EXAM_MARK(
- Stu_ID   CHAR(20),
- Course_ID  CHAR(20),
+ Stu_ID   VARCHAR(20),
+ Course_ID  VARCHAR(20),
  Quiz_1   DECIMAL(5,2),
  Quiz_2   DECIMAL(5,2),
  Quiz_3   DECIMAL(5,2),
@@ -78,7 +78,7 @@ EXAM_MARK(
 --------------------------------------------
 
 COURSE_UNIT(
- Course_ID  CHAR(20),
+ Course_ID  VARCHAR(20),
  C_Name    VARCHAR(50),
  Credit    INT,
  C_Type    VARCHAR(15),
@@ -89,7 +89,7 @@ COURSE_UNIT(
 --------------------------------------------
 
 DEPARTMENT(
- Dep_ID  CHAR(20),
+ Dep_ID  VARCHAR(20),
  D_Name VARCHAR(50),
  PRIMARY KEY(Dep_ID),
  FOREIGN KEY(DEAN_ID) REFERENCES DEAN(DEAN_ID),
@@ -98,8 +98,8 @@ DEPARTMENT(
 --------------------------------------------
 
 ATTENDENCE(
- Course_ID  CHAR(20),
- Stu_ID  CHAR(20),
+ Course_ID  VARCHAR(20),
+ Stu_ID  VARCHAR(20),
  A_Status VARCHAR(20),
  A_DATE     DATE,
  PRIMARY KEY(Course_ID, Stu_ID),
@@ -112,7 +112,7 @@ ATTENDENCE(
 -----------------------------------------
 
 MEDICAL(
- MED_ID  CHAR(20),
+ MED_ID  VARCHAR(20),
  M_Description  VARCHAR(50),
  SubmitDate  DATE,
  Stu_ID  CHAR(20),
@@ -121,15 +121,15 @@ MEDICAL(
 );
 ---------------------------------------
 ELIGIBILITY(
- ELI_ID  CHAR(20),
+ ELI_ID  VARCHAR(20),
  E_Status  VARCHAR(20),
  PRIMARY KEY(ELI_ID)
 );
 --------------------------------------
 TO_Process_Attendence(
- TO_ID  CHAR(20),
- COURSE_ID CHAR(20),
- Stu_ID CHAR(20),
+ TO_ID  VARCHAR(20),
+ COURSE_ID VARCHAR(20),
+ Stu_ID VARCHAR(20),
  PRIMARY KEY(TO_ID, Course_ID, Stu_ID),
  FOREIGN KEY(TO_ID) REFERENCES TO(TO_ID),
  FOREIGN KEY(COURSE_ID) REFERENCES COURSE_UNIT(Course_ID),
@@ -138,16 +138,16 @@ TO_Process_Attendence(
 );
 -------------------------------------
 LEC_Works_Department(
-  LEC_ID CHAR(20),
-  Dep_ID CHAR(20),
+  LEC_ID VARCHAR(20),
+  Dep_ID VARCHAR(20),
   PRIMARY KEY(LEC_ID, Dep_ID),
   FOREIGN KEY(LEC_ID) REFERENCES LECTURER(LEC_ID),
   FOREIGN KEY(Dep_ID) REFERENCES DEPARTMENT(Dep_ID)
 );
 
 LEC_Conduct_COURSE(
- LEC_ID CHAR(20),
- COURSE_ID CHAR(20),
+ LEC_ID VARCHAR(20),
+ COURSE_ID VARCHAR(20),
  Lec_in_charge VARCHAR(20),
  Lec_hours     DECIMAL(5,2),
  PRIMARY KEY(LEC_ID, Course_ID),
@@ -156,33 +156,33 @@ LEC_Conduct_COURSE(
 );
 -----------------------------------------
 LEC_Teches_STU(
- LEC_ID CHAR(20),
- Stu_ID CHAR(20),
+ LEC_ID VARCHAR(20),
+ Stu_ID VARCHAR(20),
  PRIMARY KEY(LEC_ID, Stu_ID),
  FOREIGN KEY(LEC_ID) REFERENCES LECTURER(LEC_ID),
  FOREIGN KEY(Stu_ID) REFERENCES STUDENT(Stu_ID)
 );
 ----------------------------------------
 STU_Learns_Course(
- Stu_ID CHAR(20),
- COURSE_ID CHAR(20),
+ Stu_ID VARCHAR(20),
+ COURSE_ID VARCHAR(20),
  PRIMARY KEY(Stu_ID, Course_ID),
  FOREIGN KEY(Stu_ID) REFERENCES STUDENT(Stu_ID),
  FOREIGN KEY(COURSE_ID) REFERENCES COURSE_UNIT(Course_ID)
 
 );
 STU_Has_ELIG(
- Stu_ID CHAR(20),
- ELI_ID CHAR(20),
+ Stu_ID VARCHAR(20),
+ ELI_ID VARCHAR(20),
  PRIMARY KEY(Stu_ID, ELI_ID),
  FOREIGN KEY(Stu_ID) REFERENCES STUDENT(Stu_ID),
  FOREIGN KEY(ELI_ID) REFERENCES ELIGIBILITY(ELI_ID)
 );
 ------------------------------------------
 ATTEN_Depend_ELIG(
- ELI_ID CHAR(20),
- COURSE_ID CHAR(20),
- Stu_ID CHAR(20),
+ ELI_ID VARCHAR(20),
+ COURSE_ID VARCHAR(20),
+ Stu_ID VARCHAR(20),
  PRIMARY KEY(ELI_ID, Course_ID, Stu_ID),
  FOREIGN KEY(ELI_ID) REFERENCES ELIGIBILITY(ELI_ID),
  FOREIGN KEY(COURSE_ID) REFERENCES COURSE_UNIT(Course_ID),
