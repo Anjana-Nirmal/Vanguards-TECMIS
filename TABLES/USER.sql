f6fd9cb5a4e2c87c1bcac2eff143384e2c96b25c
Vindhya-us <vindhya_20211025@fot.ruh.ac.lk>
2024-10-25 15:42:36 +0530

    USER table created

--- TABLES/USER.sql
+++ TABLES/USER.sql
@@ -1,1 +1,12 @@
-CREATE TABLE.
+Create table USER(
+ User_Id CHAR(20),
+ NIC     CHAR(20),
+ F_name  VARCHAR(50),
+ L_name  VARCHAR(50),
+ DOB      DATE ,
+ Gender   VARCHAR(10),
+ Address  VARCHAR(50),
+ Contact_no VARCHAR(20),
+ Email VARCHAR(50),
+ PRIMARY KEY(User_Id)
+);
