1f27b0bf39a565ff5a050c4feea1fa42357ccda5
Vindhya-us <vindhya_20211025@fot.ruh.ac.lk>
2024-10-26 17:27:12 +0530

    add Roles to USER

--- TABLES/USER.sql
+++ TABLES/USER.sql
@@ -1,12 +1,13 @@
 Create table USER(
- User_Id CHAR(20),
- NIC     CHAR(20),
- F_name  VARCHAR(50),
- L_name  VARCHAR(50),
- DOB      DATE ,
- Gender   VARCHAR(10),
- Address  VARCHAR(50),
- Contact_no VARCHAR(20),
- Email VARCHAR(50),
- PRIMARY KEY(User_Id)
+    User_Id CHAR(20),
+    NIC CHAR(20),
+    F_name VARCHAR(50),
+    L_name VARCHAR(50),
+    DOB DATE,
+    Gender VARCHAR(10),
+    Address VARCHAR(50),
+    Contact_no VARCHAR(20),
+    Email VARCHAR(50),
+    Roles ENUM('STUDENT', 'LECTURER', 'DEAN', 'TO'),
+    PRIMARY KEY(User_Id)
 );
