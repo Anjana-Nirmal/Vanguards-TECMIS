fad5ea0b3b6430ff372cad79fe146b82bfd31436
Vindhya-us <vindhya_20211025@fot.ruh.ac.lk>
2024-10-26 17:51:16 +0530

    set Student status

--- TABLES/USER.sql
+++ TABLES/USER.sql
@@ -11,3 +11,14 @@ Create table USER(
     Roles ENUM('STUDENT', 'LECTURER', 'DEAN', 'TO'),
     PRIMARY KEY(User_Id)
 );
+STUDENT(                                  --STUDENT Table structure
+    Stu_ID VARCHAR(20),
+    S_Status ENUM('proper','repeat','suspended'),
+    GPA DECIMAL(4, 2),
+    Dep_ID VARCHAR(20),
+    ELI_ID VARCHAR(20),
+    PRIMARY KEY(Stu_ID),
+    FOREIGN KEY(Stu_ID) REFERENCES USER(User_Id),
+    FOREIGN KEY(Dep_ID) REFERENCES DEPARTMENT(Dep_ID),
+    FOREIGN KEY(ELI_ID) REFERENCES ELIGIBILITY(ELI_ID)
+);
