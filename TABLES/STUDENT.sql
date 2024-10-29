1f3388b699dbd7a8b3dc198719ee72715f1e20ca
Vindhya-us <vindhya_20211025@fot.ruh.ac.lk>
2024-10-29 20:26:38 +0530

    create separate sql file for student

---
+++ TABLES/STUDENT.sql
@@ -0,0 +1,11 @@
+CREATE TABLE STUDENT(                       --STUDENT Table structure
+    Stu_ID VARCHAR(20),
+    S_Status ENUM('proper', 'repeat', 'suspended'),
+    GPA DECIMAL(4, 2),
+    Dep_ID VARCHAR(20),
+    ELI_ID VARCHAR(20),
+    PRIMARY KEY(Stu_ID),
+    FOREIGN KEY(Stu_ID) REFERENCES USER(User_Id),
+    FOREIGN KEY(Dep_ID) REFERENCES DEPARTMENT(Dep_ID),
+    FOREIGN KEY(ELI_ID) REFERENCES ELIGIBILITY(ELI_ID)
+);
--- TABLES/USER.sql
+++ TABLES/USER.sql
@@ -12,14 +12,5 @@ CREATE TABLE USER(                           --USER Table structure
     PRIMARY KEY(User_Id)
 );
 
-CREATE TABLE STUDENT(                       --STUDENT Table structure
-    Stu_ID VARCHAR(20),
-    S_Status ENUM('proper', 'repeat', 'suspended'),
-    GPA DECIMAL(4, 2),
-    Dep_ID VARCHAR(20),
-    ELI_ID VARCHAR(20),
-    PRIMARY KEY(Stu_ID),
-    FOREIGN KEY(Stu_ID) REFERENCES USER(User_Id),
-    FOREIGN KEY(Dep_ID) REFERENCES DEPARTMENT(Dep_ID),
-    FOREIGN KEY(ELI_ID) REFERENCES ELIGIBILITY(ELI_ID)
-);
+
+
